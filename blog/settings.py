21fd29f8914e8bbec72296988876c316712c97d5
zannatulaman <shalu_us@yahoo.com>
2023-09-24 06:41:59 -0400

    added requirements.txt

--- blog/settings.py
+++ blog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,8 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+
+
 STATIC_URL = 'static/'
-import os
+
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static')
 ]
